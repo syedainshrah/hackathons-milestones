b2dfc7ba268c82c8d95d49262b020b344b2354d5
Syedainshrah <161240095+syedainshrah@users.noreply.github.com>
2024-09-11 18:03:00 +0500

    Add files via upload

---
+++ hackathon2/milestone1/script.ts
@@ -0,0 +1,13 @@
+const toggleButton = document.getElementById('toggle -skills') as HTMLElement
+const skills = document.getElementById('skills') as HTMLElement
+
+toggleButton.addEventListener('click', ()=>  {
+if (skills.style.display === 'none'){
+    skills.style.display = 'block'
+}else {
+    skills.style.display = 'none'
+}
+
+  
+}
+);
